e04a5b3019871ea0ab683e2aa6bd19b8c2cffdfc
willmaddock <122849974+willmaddock@users.noreply.github.com>
2025-08-12 13:29:28 -0600

    Deploy

--- config/importmap.rb
+++ config/importmap.rb
@@ -12,7 +12,7 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # Pin Bootstrap JavaScript bundle (which includes Popper)
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js", preload: true
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js", preload: true
 
-# ✅ Pin the dark mode toggle script explicitly under app/javascript
+# Pin the dark mode toggle script explicitly under app/javascript
 pin "dark_mode_toggle", to: "dark_mode_toggle.js"
